1f549e9d126f4c94010254116911d25746fb523e
romulocrp <romulo.crp97@gmail.com>
2024-09-15 20:56:17 -0300

    Renewed Website, changes are: cards in cards out output, switch button new functionalities, value error messages

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-const deck1 = document.getElementById("deck-input1").value;
-const deck2 = document.getElementById("deck-input2").value;
+state = {
+    counter: 0
+}    
+
+function emptyDeckHandler (deck) {
+    if (deck === "") {
+        return true
+    }
+    return false
+}    
+
+function outOfPatternHandler (deck) {
+    deckArray = deck.split("\n")
+    re = /^(?!\d+ .)+/;
+    for (i = 0; i < deckArray.length; i++) {
+        if (deckArray[i].length === 0) continue;
+        testCardEnsemble = re.test(deckArray[i]);
+        if (testCardEnsemble) {
+            return [true, deckArray[i]]
+        }
+    }
+    return [false, null]
+}   
+
+function floatingCardQtd (deck) {
+    deckArray = deck.split("\n");
+    for (i=0; i < deckArray.length; i++) {
+        if (deckArray[i].length === 0) continue;
+        strValue = deckArray[i][1];
+        if (strValue === "," || strValue === ".") {
+            return [true, deckArray[i]]
+        }
+    }
+    return [false, null]
+}    
 
 function deckMapping(deck) {
     var deckArray = deck.split("\n");
@@ -18,21 +51,43 @@ function comparingDecks(deck1, deck2) {
     objDeck1 = deckMapping(deck1);
     objDeck2 = deckMapping(deck2);
     missingCards = Object();
-    pivotingDeck = Object();
     for (let j = 0; j < Object.keys(objDeck2).length; j++) {
-        if (Object.keys(objDeck1).includes(Object.keys(objDeck2)[j]) === false) {
+        if (objDeck1[Object.keys(objDeck2)[j]] === undefined) {
             missingCards[Object.keys(objDeck2)[j]] = objDeck2[Object.keys(objDeck2)[j]];
         } else {
             missingCards[Object.keys(objDeck2)[j]] = objDeck2[Object.keys(objDeck2)[j]] - objDeck1[Object.keys(objDeck2)[j]];
         }
     }    
-    for (let z = 0; z < Object.keys(missingCards).length; z++) {
-        if (missingCards[Object.keys(missingCards)[z]] >= 1) {
-           pivotingDeck[Object.keys(missingCards)[z]] = missingCards[Object.keys(missingCards)[z]] 
-        }
-    
+    return missingCards;
+}    
+
+function sideInFilter(deck) {
+    sideInCards = Object();
+    for (let z = 0; z < Object.keys(deck).length; z++) {
+        if (deck[Object.keys(deck)[z]] > 0) {
+            sideInCards[Object.keys(deck)[z]] = deck[Object.keys(deck)[z]];
+        }    
     }
-    return pivotingDeck;
+    return sideInCards;
+}    
+
+function sideOutFilter(deck) {
+    sideOutCards = Object();
+    for (let t = 0; t < Object.keys(deck).length; t++) {
+        if (deck[Object.keys(deck)[t]] < 0) {
+            sideOutCards[Object.keys(deck)[t]] = -deck[Object.keys(deck)[t]];
+        }    
+    }
+    return sideOutCards;
+}    
+
+function switcher(deck1, deck2) {
+    const comparedDecks = comparingDecks(deck1, deck2);
+    const sideIn = sideInFilter(comparedDecks);
+    const sideOut = sideOutFilter(comparedDecks);
+    const sideOutMissed = comparingDecks(deck2, deck1);
+    const sideOutMissedCards = sideInFilter(sideOutMissed);
+    return [sideIn, sideOut, sideOutMissedCards]
 }    
 
 function deckToHtml (deck) {
@@ -44,8 +99,48 @@ function deckToHtml (deck) {
 }    
 
 function printResult () {
-    document.getElementById("result-1-2").innerHTML = `<b>LISTA 1 PARA LISTA 2</b><br>${deckToHtml(comparingDecks(deck1, deck2))}`;
-    document.getElementById("result-2-1").innerHTML = `<b>LISTA 2 PARA LISTA 1</b><br>${deckToHtml(comparingDecks(deck2, deck1))}`;
+    
+    const deck1 = document.getElementById("deck-input1").value;
+    const deck2 = document.getElementById("deck-input2").value;
+    const deck1Check = emptyDeckHandler(deck1);
+    const deck2Check = emptyDeckHandler(deck2);
+    const outPatternEnsemble1 = outOfPatternHandler(deck1);
+    const outPatternEnsemble2 = outOfPatternHandler(deck2);
+    const floatingQtd1 = floatingCardQtd(deck1);
+    const floatingQtd2 = floatingCardQtd(deck2);
+    
+    if (deck1Check || deck2Check) {
+        document.getElementById("switch-result").innerHTML = "<p><b>Algum deck está vazio!</b></p>";
+        return 0
+    }
+
+    if (outPatternEnsemble1[0]) {
+        document.getElementById("switch-result").innerHTML = `<p>Cartas fora do padrão:<br>${outPatternEnsemble1[1]}</p>`;
+        return 0
+    } else if (outPatternEnsemble2[0]) {
+        document.getElementById("switch-result").innerHTML = `<p>Cartas fora do padrão:<br>${outPatternEnsemble2[1]}</p>`;
+        return 0
+    }
+
+    if (floatingQtd1[0]) {
+        document.getElementById("switch-result").innerHTML = `<p>Cartas fracionadas:<br>${floatingQtd1[1]}</p>`;
+        return 0
+    } else if (floatingQtd2[0]) {
+        document.getElementById("switch-result").innerHTML = `<p>Cartas fracionadas:<br>${floatingQtd2[1]}</p>`;
+        return 0
+    }    
+
+    if (state.counter === 0){
+        const change = switcher(deck1, deck2);
+        document.getElementById("switch-result").innerHTML = `<b>LISTA 1 PARA LISTA 2</b><br><i>Entra:</i><br>${deckToHtml(change[0])}<br><i>Sai:</i><br>${deckToHtml(change[1])}${deckToHtml(change[2])}`;
+        document.getElementById("switch-button").innerText = "<Switch!"
+        state.counter += 1
+    } else {
+        const change = switcher(deck2, deck1);
+        document.getElementById("switch-result").innerHTML = `<b>LISTA 2 PARA LISTA 1</b><br><i>Entra:</i><br>${deckToHtml(change[0])}<br><i>Sai:</i><br>${deckToHtml(change[1])}${deckToHtml(change[2])}`;
+        document.getElementById("switch-button").innerText = "Switch!>"
+        state.counter -= 1
+    }    
 }
 
 
